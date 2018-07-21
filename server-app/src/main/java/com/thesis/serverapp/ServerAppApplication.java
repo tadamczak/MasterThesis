2ec758a1abd33da795485e9e5b54117828ca78f2
tadamczak <t.adamczak@padkom.pl>
2018-07-21 17:17:01 +0200

    Correct spring boot application implemented

--- server-app/src/main/java/com/thesis/serverapp/ServerAppApplication.java
+++ server-app/src/main/java/com/thesis/serverapp/ServerAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerAppApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServerAppApplication.class, args);
+  }
 }
